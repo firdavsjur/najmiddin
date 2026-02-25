a72566a2a9e3938ec9b72129631d2e516a1b59e2
Firdavs <firdavsforme@gmail.com>
2026-02-25 12:52:09 +0500

    adminka > orders > get request: date filter qoshish kk (backend)

--- routes/admin/orders.js
+++ routes/admin/orders.js
@@ -79,13 +79,15 @@ router.use(isAdmin);
  *         schema:
  *           type: string
  *           format: date-time
- *         description: Filter orders from this date (ISO8601, inclusive start of day UTC)
+ *           example: '2025-01-01T00:00:00.000Z'
+ *         description: Filter orders from this date (ISO8601 or YYYY-MM-DD, inclusive start of day UTC)
  *       - in: query
  *         name: dateTo
  *         schema:
  *           type: string
  *           format: date-time
- *         description: Filter orders up to this date (ISO8601, inclusive end of day UTC)
+ *           example: '2025-01-31T23:59:59.999Z'
+ *         description: Filter orders up to this date (ISO8601 or YYYY-MM-DD, inclusive end of day UTC)
  *     responses:
  *       200:
  *         description: List of orders
